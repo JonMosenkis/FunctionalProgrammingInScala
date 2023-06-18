84edb9b5e13dc602e2178fa7b57a94178616b042
JonMosenkis <jonathanmos@wix.com>
2023-06-18 22:02:03 +0300

    more list methods

--- FunctionalProgrammingInScala/src/main/scala/Chapter3/SimpleList.scala
+++ FunctionalProgrammingInScala/src/main/scala/Chapter3/SimpleList.scala
@@ -33,3 +33,28 @@ object SimpleList:
     case Cons(head, tail) =>
       if predicate(head) then dropWhile(tail, predicate)
       else Cons(head, dropWhile(tail, predicate))
+
+  def init[A](list: SimpleList[A]): SimpleList[A] = list match
+    case Cons(_, Nil) => Nil
+    case Cons(head, tail) => Cons(head, init(tail))
+    case Nil => Nil
+
+  def foldRight[A, B](list: SimpleList[A], acc: B, f: (A, B) => B): B = foldLeft(reverse(list), acc, (a, b) => f(b, a))
+
+  @tailrec
+  def foldLeft[A, B](list: SimpleList[A], acc: B, f: (B, A) => B): B = list match
+    case Nil => acc
+    case Cons(head, tail) => foldLeft(tail, f(acc, head), f)
+
+  def length[A](list: SimpleList[A]): Int = foldLeft(list, 0, (acc, _) => acc + 1)
+
+  def sum(list: SimpleList[Int]): Int = foldLeft(list, 0, _ + _)
+
+  def product(list: SimpleList[Double]): Double = foldLeft(list, 1.0, _ * _)
+
+  def reverse[A](list: SimpleList[A]): SimpleList[A] = foldLeft(list, Nil: SimpleList[A], (a, b) => Cons(b, a))
+
+  def append[A](listA: SimpleList[A], listB: SimpleList[A]): SimpleList[A] =
+    foldRight(listA, listB, (head, tail) => Cons(head, tail))
+
+  def concat[A](lists: SimpleList[SimpleList[A]]): SimpleList[A] = foldLeft(lists, Nil: SimpleList[A], append)
--- FunctionalProgrammingInScala/src/test/scala/Chapter3/SimpleListTest.scala
+++ FunctionalProgrammingInScala/src/test/scala/Chapter3/SimpleListTest.scala
@@ -12,6 +12,42 @@ class SimpleListTest extends Specification {
     "dropWhile" >> {
       SimpleList.dropWhile(SimpleList(1, 2, 3, 4), a => a % 2 == 0) must beEqualTo(SimpleList(1 ,3))
     }
+
+    "pop" >> {
+      SimpleList.init(SimpleList(1, 2, 3, 4)) must beEqualTo(SimpleList(1, 2, 3))
+    }
+
+    "calculate length" >> {
+      SimpleList.length(SimpleList("a", "b", "c", "c")) must beEqualTo(4)
+    }
+
+    "foldLeft" >> {
+      SimpleList.foldLeft(SimpleList(3, 2, 1), 6, _ - _) must beEqualTo(0)
+    }
+
+    "foldRight" >> {
+      SimpleList.foldRight(SimpleList(1, 2, 3), Nil: SimpleList[Int], Cons(_, _)) must beEqualTo(SimpleList(1, 2, 3))
+    }
+
+    "sum" >> {
+      SimpleList.sum(SimpleList(1, 2, 3)) must beEqualTo(6)
+    }
+
+    "product" >> {
+      SimpleList.product(SimpleList(2.0, 2.0, 3.0)) must beEqualTo(12.0)
+    }
+
+    "reverse" >> {
+      SimpleList.reverse(SimpleList('a', 'b', 'c')) must beEqualTo(SimpleList('c', 'b', 'a'))
+    }
+
+    "append" >> {
+      SimpleList.append(SimpleList(1, 2, 3), SimpleList(4, 5, 6)) must beEqualTo(SimpleList(1, 2, 3, 4, 5, 6))
+    }
+
+    "concat" >> {
+      SimpleList.concat(SimpleList(SimpleList(1, 2), SimpleList(3, 4), SimpleList(5, 6))) must beEqualTo(SimpleList(1, 2, 3, 4, 5, 6))
+    }
   }
 
 
