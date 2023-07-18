4feae73048a3e6e5ee777f0429b68244dc070323
JonMosenkis <jonathanmos@wix.com>
2023-07-18 18:56:53 +0300

    State

---
+++ FunctionalProgrammingInScala/src/main/scala/Chapter6/State.scala
@@ -0,0 +1,28 @@
+package Chapter6
+
+
+opaque type State[S, +A] = S => (A, S)
+
+object State {
+  extension[S, A](underlying: State[S, A])
+    def run(s: S): (A, S) = underlying(s)
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] =
+      s =>
+        val (a, s1) = underlying(s)
+        f(a)(s1)
+
+    def map[B](f: A => B): State[S, B] =
+      underlying.flatMap(a => unit(f(a)))
+
+    def map2[B, C](other: State[S, B])(f: (A, B) => C): State[S, C] =
+      for
+        a <- underlying
+        b <- other
+      yield f(a, b)
+
+  def apply[S, A](f: S => (A, S)): State[S, A] = f
+
+  def unit[S, A](a: A): State[S, A] = (s: S) => (a, s)
+}
+
