acf041b39e0f6b3de37b31ddaee233678e6eac75
JonMosenkis <jonathanmos@wix.com>
2023-08-29 02:22:13 +0300

    Par

---
+++ FunctionalProgrammingInScala/src/main/scala/Chapter7/Par.scala
@@ -0,0 +1,34 @@
+package Chapter7
+
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
+opaque type Par[A] = ExecutorService => Future[A]
+
+object Par {
+  def unit[A](a: => A): Par[A] = es => UnitFuture(a)
+  def fork[A](pa: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call: A =
+        pa(es).get
+    })
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  extension [A](pa: Par[A]) def run(executorService: ExecutorService): Future[A] = pa(executorService)
+
+  extension [A](pa: Par[A]) def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) =>
+      val futureA = pa(es)
+      val futureB = pb(es)
+      UnitFuture(f(futureA.get, futureB.get))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  private case class UnitFuture[A](get: A) extends Future[A]:
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+    override def isCancelled: Boolean = false
+    override def isDone: Boolean = true
+    override def get(timeout: Long, unit: TimeUnit): A = get
+}
+
+
