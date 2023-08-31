395f13403bc78a70ff973825e00b42830fa778e8
JonMosenkis <jonathanmos@wix.com>
2023-09-01 00:03:06 +0300

    Par Complete

--- FunctionalProgrammingInScala/src/main/scala/Chapter7/Par.scala
+++ FunctionalProgrammingInScala/src/main/scala/Chapter7/Par.scala
@@ -19,7 +19,9 @@ object Par {
     (es: ExecutorService) =>
       val futureA = pa(es)
       val futureB = pb(es)
-      UnitFuture(f(futureA.get, futureB.get))
+      val result = UnitFuture(f(futureA.get, futureB.get))
+      result
+
 
   def asyncF[A, B](f: A => B): A => Par[B] =
     a => lazyUnit(f(a))
@@ -28,7 +30,14 @@ object Par {
     pa.map2(unit(()))((a, _) => f(a))
 
   def sequence[A](ps: List[Par[A]]): Par[List[A]] =
-    ps.foldRight(unit(List.empty[A]))((pa, acc) => pa.map2(acc)(_ :: _))
+    sequenceBalanced(ps.toIndexedSeq).map(_.toList)
+
+  def sequenceBalanced[A](pas: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] =
+    if pas.isEmpty then unit(IndexedSeq.empty)
+    else if pas.size == 1 then pas.head.map(a => IndexedSeq(a))
+    else
+      val (l, r) = pas.splitAt(pas.size / 2)
+      sequenceBalanced(l).map2(sequenceBalanced(r))(_ ++ _)
 
   def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] =
     fork {
@@ -41,6 +50,33 @@ object Par {
       val pars: List[Par[Option[A]]] = as.map(asyncF(a => Some(a).filter(f)))
       sequence(pars).map(_.flatten)
 
+  def countWords(paragraphs: List[String]): Par[Int] =
+    def wordsInParagraph(p: String): Int =
+      p.split(' ').length
+
+    val parWords: List[Par[Int]] = paragraphs.map(asyncF(wordsInParagraph))
+    sequence(parWords).map(_.sum)
+
+  def map3[A, B, C, D](pa: Par[A], pb: Par[B], pc: Par[C])(f: (A, B, C) => D): Par[D] =
+    pa.map2(pb)((a, b) => (a, b)).map2(pc)((ab, c) => f(ab._1, ab._2, c))
+
+  def choiceN[A](np: Par[Int])(choices: List[Par[A]]): Par[A] = es =>
+    val n = np(es).get()
+    choices(n)(es)
+
+  def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    choiceN(cond.map(c => if c then 1 else 0))(List(t, f))
+
+  def choiceMap[K, V](key: Par[K])(choices: Map[K, Par[V]]): Par[V] = es =>
+    val k = key(es).get()
+    choices(k)(es)
+
+  extension [A](pa: Par[A]) def flatMap[B](f: A => Par[B]): Par[B] = es =>
+    val a = pa(es).get()
+    f(a)(es)
+
+  def join[A](ppa: Par[Par[A]]): Par[A] = es =>
+    ppa(es).get()(es)
 
   private case class UnitFuture[A](get: A) extends Future[A]:
     override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
